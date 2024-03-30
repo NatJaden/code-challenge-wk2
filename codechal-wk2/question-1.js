9fadb3ec9dd08a8b31399e6a0964fa946a83d028
Nathaniel <nathaniel.jaden@student.moringaschool.com>
2024-03-30 13:52:14 +0300

    fixed a few errors

--- codechal-wk2/question-1.js
+++ codechal-wk2/question-1.js
@@ -1,16 +1,17 @@
 function swapCase(str) {
   return str
-    .split("")
+    .split("") // splits the string into characters
     .map((charcter) => {
       if (charcter === charcter.toUpperCase()) {
-        return charcter.toLowerCase();
+        return charcter.toLowerCase(); //Changes the charcters to lowercase if they are in uppercase
       } else if (charcter === charcter.toLowerCase()) {
-        return charcter.toUpperCase();
+        return charcter.toUpperCase(); //Changes the charcters to lowercase if they are in uppercase
       }
-      return charcter;
+      return charcter; //Returns each character
     })
-    .join("");
+    .join(""); // Joins every character back into a string
 }
 const test = "The Quick Brown Fox";
-const outTest = swapCase(test);
-console.log(outTest);
+const testOuput = swapCase(test);
+console.log(outTest); //Prints the output where every chracters case is changer to the opposite case
+
--- codechal-wk2/question-2.js
+++ codechal-wk2/question-2.js
@@ -1,12 +1,13 @@
+//Accepts two numbers
 function generetedNumberRange(num1, num2) {
-  const generetedArr = [];
+  const generetedArr = []; //stores the generated numbers
   if (num1 >= num2) {
     for (let i = num1; i >= num2; i--) {
-      generetedArr.push(i);
+      generetedArr.push(i); // If num1 is greater than or equal to num2, generate numbers in descending order
     }
   } else {
     for (let i = num1; i <= num2; i++) {
-      generetedArr.push(i);
+      generetedArr.push(i); // If num2 is greater than or equal to num1, generate numbers in ascending order
     }
   }
   return generetedArr;
